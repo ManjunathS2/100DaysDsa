facf95a4efa6a711e082ed858aec145eb7e922d0
Manjunath <manjunath8885@gmail.com>
2026-02-28 10:53:43 +0530

    recursion

---
+++ Dsa/src/recursion/fibonacci.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class fibonacci {
+	public static int fibonacci(int n) {
+		if(n<=1) {
+			return n;			
+		}
+		return fibonacci(n-1)+fibonacci(n-2);
+		
+	}
+
+	public static void main(String[] args) {
+		int n=4;
+		System.out.print(fibonacci(n));
+	}
+
+}

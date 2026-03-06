45a1c726f6b963f9062b5c20d8c6cf46d5ba3b28
Manjunath <manjunath8885@gmail.com>
2026-03-06 16:01:02 +0530

    LL

---
+++ Dsa/src/LinkedList/Search.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+class Node {
+	int data;
+	Node next;
+	
+	Node(int data1,Node next1){
+		data=data1;
+		next=next1;
+	}
+	
+	//if only data
+	Node(int data1){
+		data=data1;
+		next=null;			
+	}
+}
+
+public class Search {
+	public static Node convertArraytoLL(int[] arr) {
+		Node head=new Node(arr[0]);
+		Node mover=head;
+		for(int i=1;i<arr.length;i++) {
+			Node temp=new Node(arr[i]);
+			mover.next=temp;//creating link
+			mover=temp;
+		}
+		return head;
+		
+	}
+	
+	public static int length(Node head,int k) {
+		Node temp=head;
+		while(temp!=null) {
+			if(temp.data==k) return 1;
+			temp=temp.next;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+        int[] arr= {1,2,4,5};
+		
+		//traverse
+		Node head=convertArraytoLL(arr);
+		System.out.print(length(head,15));
+		
+
+
+	}
+
+}
